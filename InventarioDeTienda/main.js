1f0644c0cafe42b0fb4260ed4b05541ca8239133
Vespertino <Vespertino@C04PC16.informatica.comercio>
2025-11-10 21:42:19 +0100

    Modified: ejercicios js

--- InventarioDeTienda/main.js
+++ InventarioDeTienda/main.js
@@ -82,11 +82,11 @@ function agregarProducto(nombre, precioBase, categoria) {
   guardarEnLocalStorage();
   mostrarProductos();
 }
+
 buttonLimpiarLocal.addEventListener("click", function (e) {
   e.preventDefault();
   localStorage.removeItem("arrayProductos");
 });
-
 // Ejecutamos la carga inicial al arrancar
 cargarEnLocalStorage();
 mostrarProductos();
