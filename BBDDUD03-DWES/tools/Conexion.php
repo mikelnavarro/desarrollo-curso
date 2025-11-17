998a551ae7013809af113ef2bfbc2ff4fd104fbd
Mikel <mikelnaval2006@gmail.com>
2025-11-17 18:37:10 +0100

    Unidad BD Composer DWES

--- BBDDUD03-DWES/tools/Conexion.php
+++ BBDDUD03-DWES/tools/Conexion.php
@@ -4,6 +4,19 @@
  * Lo más profesional es leer los datos de un archivo "config.ini"
  * o "config.json" los parámetros necesarios para la conexión.
  */
- 
- 
+
+$servername = "localhost";
+$username = "dweb";
+$password = "12345";
+$dbname = "desarollowebentornoservidor";
+
+
+
+
+try {
+  $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+  $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch(PDOException $e) {
+  echo "Connection failed: " . $e->getMessage();
+}
  ?>
