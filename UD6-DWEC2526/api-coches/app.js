dd131c11bed1904e3412dfda55f5de168cff335c
mikelnaval2006@gmail.com <mikelnaval2006@gmail.com>
2026-02-03 10:04:42 +0100

    pruebitas commit

--- UD6-DWEC2526/api-coches/app.js
+++ UD6-DWEC2526/api-coches/app.js
@@ -3,31 +3,25 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-require("dotenv").config();
-const cors = require('cors');
-const { connectDB } = require("./config/db");
-connectDB();
+var usersRouter = require('./routes/users');
+var carsRouter = require('./routes/cars');
+var auth = require('./routes/auth');
 
-const carsRouter = require('./routes/cars');
-const authentication = require('./routes/auth');
+
 var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
-
-// Middleware auto
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-
-// Routes
-// index.js
-app.use('/api/auth', authentication);
-app.use('/api/cars', carsRouter);
+app.use('/users', usersRouter);
+app.use('/cars', carsRouter);
+app.use('/auth', auth);
 
 
 // catch 404 and forward to error handler
