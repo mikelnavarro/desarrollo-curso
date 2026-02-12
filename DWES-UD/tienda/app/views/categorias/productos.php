bf2302ff7d8b9e58d8db2a2086826f10b9477fee
mikelnaval2006@gmail.com <mikelnaval2006@gmail.com>
2026-02-12 16:18:39 +0100

    Tienda App

--- DWES-UD/tienda/app/views/categorias/productos.php
+++ DWES-UD/tienda/app/views/categorias/productos.php
@@ -15,7 +15,43 @@
         </div>
     <?php endforeach; ?>
 <?php endif; ?>
-
-
+<table>
+    <thead>
+        <tr>
+            <th>Nombre</th>
+            <th>Precio</th>
+            <th>Acción</th>
+        </tr>
+    </thead>
+    <tbody>
+    <?php foreach($datos['productos'] as $prod) : ?>
+    <tr>
+            <td><?= $prod['Nombre']; ?></td>
+            <td><?= $prod['Precio']; ?></td>
+            <td><a href="<?= RUTA_URL ?>/Carrito/agregar/<?= $prod['CodProd']; ?>" class="btn-comprar">Añadir al carrito</a></td>
+        </tr>
+    <?php endforeach; ?>
+    </tbody>
+</table>
+<style>
+    body {
+        background-color: #f1f1f1;
+    }
+    table {
+        margin: auto;
+        border-collapse: collapse;
+        width: 50%;
+    }
+    thead tr {
+        background-color: #000000;
+        color: #ffffff;
+        padding: 1rem;
+        font-weight: bold;
+        text-align: center;
+    }
+    tbody tr td {
+        background-color: #ffffff;
+        padding: 1rem;
+    }
+</style>
 <a href="<?= RUTA_URL ?>/Categorias">Volver atrás</a>
-
