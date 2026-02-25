348f01e2cb203916c74dd1f35b145db2dec569a0
Mikel Navarro <mikelnaval2006@gmail.com>
2026-02-25 16:39:57 +0100

    modificacion js - asignatura dwec

--- UD5-DWEC2526/Ejercicio-P4/login.js
+++ UD5-DWEC2526/Ejercicio-P4/login.js
@@ -5,13 +5,15 @@ import { User } from "./User.js";
 const formularioLogin = document.getElementById("formLogin");
 const errorMsg = document.getElementById("errorMsg");
 
-
 const usuarioPrueba = User.create("Admin", "admin@gmail.com", "12345");
-Storage.pushToCollection("usuarios",usuarioPrueba);
-localStorage.setItem("session", JSON.stringify({
-  nombre: usuarioPrueba.nombre,
-  email: usuarioPrueba.email,
-}));
+Storage.pushToCollection("usuarios", usuarioPrueba);
+localStorage.setItem(
+  "session",
+  JSON.stringify({
+    nombre: usuarioPrueba.nombre,
+    email: usuarioPrueba.email,
+  }),
+);
 // escucha formulario
 formularioLogin.addEventListener("submit", (e) => {
   e.preventDefault();
@@ -27,10 +29,13 @@ formularioLogin.addEventListener("submit", (e) => {
   const user = User.authenticate(email, password);
   if (user) {
     window.location.href = "form.html";
-    localStorage.setItem("session", JSON.stringify({
+    localStorage.setItem(
+      "session",
+      JSON.stringify({
         nombre: user.nombre,
         email: user.email,
-      }));
+      }),
+    );
   } else {
     const newUser = User.create(nombre, email, password);
     localStorage.setItem(
