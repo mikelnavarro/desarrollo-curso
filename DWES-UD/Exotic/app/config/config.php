f9e7f5afd88349c894ce81c96e83c2e20b263d69
mikelnaval2006@gmail.com <mikelnaval2006@gmail.com>
2026-03-10 13:41:03 +0100

    Exotic App DWESS

---
+++ DWES-UD/Exotic/app/config/config.php
@@ -0,0 +1,23 @@
+<?php
+// Base de datos
+define('DB_HOST', 'localhost');
+define('DB_NAME', 'exotic');
+define('DB_USER', 'dweb');
+define('DB_PASS', '12345');
+define('DB_CHARSET', 'utf8');
+
+// ruta
+define("RUTA_URL", "http://localhost/desarrollo-curso/DWES-UD/Exotic");
+define('RUTA_APP', dirname(dirname(__FILE__)));
+
+// Datos de Configuración de Correo GMAIL
+define('MAIL_HOST', 'smtp.gmail.com');
+define('MAIL_PORT', '587');
+define('MAIL_USER', 'mikelnavarro33@gmail.com');
+define('MAIL_PASS', 'szyx ldbc ebqr fpbi');
+define('MAIL_FROM', 'mikelnavarro33@gmail.com');
+define('MAIL_CHARSET', 'UTF-8');
+define('MAIL_FROMNAME', 'Tienda');
+
+// nombre
+define('NOMBRESITIO', 'Exotic Aplicación');
---
+++ DWES-UD/Exotic/app/tools/Mailer.php
@@ -0,0 +1,53 @@
+<?php
+namespace Mikelnavarro\App;
+use PHPMailer\PHPMailer\Exception;
+use PHPMailer\PHPMailer\PHPMailer;
+
+class Mailer
+{
+    // Atributos de Correo
+    private $mail;
+
+    public function __construct() {
+        $this->mail = new PHPMailer(true);
+
+        // Configuración usando las constantes de config.php
+        $this->mail->isSMTP();
+        $this->mail->Host       = MAIL_HOST;
+        $this->mail->SMTPAuth   = true;
+        $this->mail->Username   = MAIL_USER;
+        $this->mail->Password   = MAIL_PASS;
+        $this->mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
+        $this->mail->Port       = MAIL_PORT;
+        $this->mail->CharSet = 'UTF-8';
+        $this->mail->setFrom(MAIL_FROM, MAIL_FROMNAME);
+
+    }
+	public function send(string $to, $idPedido, array $itemsCarrito, array $resumen, array $envio, string $alt = null)
+	{
+        try {
+            // Direccion de correo electronico
+			$this->mail->addAddress($to);
+            // Asunto del Correo
+            $this->mail->Subject = "Confirmación de Pedido Tienda";
+
+            // Construcción del cuerpo del mensaje
+            $html = "<h4>¡Gracias por tu compra, {$to}!</h4> <p>Tu pedido <strong>$idPedido</strong> ha sido procesado</p>";
+            $html .= "<p><b>Método de Pago: - {$envio["metodo"]}</b></p>";
+            $html .= "<p><strong>Cantidad de artículos: {$resumen['cantidad_articulos']}</strong></b>";
+            $html .= "<p><strong>Total a pagar:</strong>{$resumen['total']}</p>";
+            $html .= "<p><b>Dirección de Envío: {$envio["direccion"]}</b></p>";
+            // es html
+			$this->mail->isHTML(true);
+            // cuerpo del mensaje
+			$this->mail->Body = $html;
+            // el cuerpo alternativo
+			$this->mail->AltBody = $alt ?? strip_tags($html);
+			$this->mail->send();
+			return true;
+		} catch (Exception $e) {
+			return $this->mail->ErrorInfo ?: $e->getMessage();
+            error_log("Error de correo: " . $mail->ErrorInfo);
+		}
+	}
+}
